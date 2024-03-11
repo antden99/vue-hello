bfcc269bed4c8f911eeda9035f0d432879707786
antden99 <antoniodenigris@icloud.com>
2024-03-11 12:04:12 +0100

    add message

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("Tutto ok");
+
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Ciao Antonio!'
+        }
+    }
+}).mount('#start_Vue')
+
